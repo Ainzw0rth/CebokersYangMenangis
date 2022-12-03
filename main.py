be2905501838892b7d84b3042941917b57ba8348
louis <louiscaesapdf@gmail.com>
2022-12-03 08:13:12 +0700

    console

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def parseFile(filename):
     print("Variable valid: ", variableValid)
 
     # # Get The CNF
-    CFG = cfg_from_file("grammarbener.txt")
+    CFG = cfg_from_file("grammarNew.txt")
     V = CFG_to_CNF(CFG[2])
     CNF = (CFG[0], CFG[1], V, CFG[3])
     
